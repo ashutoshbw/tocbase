bd71ffdbc61aba514f1e49c4bfb40bef15abddbf
Ashutosh Biswas <ashutoshbw314@gmail.com>
2022-08-20 19:38:04 +0600

    renamed spawn to proxy

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { tocPleaseCore } from './core.js';
 export function tocPlease (g = {}) {
   // querySelector
 
-  const spawnElt = $("#" + g.spawnID);
+  const proxyElt = $("#" + g.proxyID);
 
   // From now config is the merged config
-  const mergedConfig = Object.assign({}, g.config, JSON.parse(spawnElt?.innerHTML.trim() || "{}"));
+  const mergedConfig = Object.assign({}, g.config, JSON.parse(proxyElt?.innerHTML.trim() || "{}"));
 
   const headings = getHeadings(g.getFrom, g.omit, mergedConfig?.omit);
   const toc = tocPleaseCore(headings, mergedConfig);
 
   if (!toc) return;
 
-  spawnElt?.replaceWith(toc);
+  proxyElt?.replaceWith(toc);
 
   return toc;
 }
